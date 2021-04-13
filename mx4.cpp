c5e955ee4c9f041b97d6c7101d5b5ef87eb602a8
Ron Evans <ron@hybridgroup.com>
2021-04-13 19:24:59 +0200

    mx4: add wrapper for mx4 metadata

--- mx4.cpp
+++ mx4.cpp
@@ -2,11 +2,9 @@
 #include "mx4.h"
 
 MX4MetadataWrapper GetMX4MetadataFromBuffer(SapBufferWrapper buf, int width, int height) {
-	int numRead = 0;
+	int numRead;
 	MX4MetadataWrapper metadata = new MX4MetadataWrapper;
 	buf->ReadLine(width, height-1, width+64-1, height-1, metadata, &numRead);
-	int fc = metadata.frameCounter;
-	int lc = metadata.lineCount;
 	return metadata;
 }
 
