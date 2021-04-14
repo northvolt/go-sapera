ed92d8d158ff16e4f5ba1f29e5d20a1c8abb5547
Ron Evans <ron@hybridgroup.com>
2021-04-14 20:23:52 +0200

    core: set display status mode to log

--- sap.cpp
+++ sap.cpp
@@ -6,28 +6,16 @@ extern "C"
 	{
 		goxferhandler(pInfo);
 	}
-
-	CORSTATUS DisplayStatus(char *functionName, CORSTATUS status)
-	{
-		if (status != CORSTATUS_OK) {
-			char szId[128], szInfo[128], szLevel[64], szModule[64];
-			CorManGetStatusTextEx(status, szId, sizeof(szId), szInfo, sizeof(szInfo), szLevel, sizeof(szLevel), szModule, sizeof(szModule));
-			printf("%s in \"%s\" <%s module> => %s (%s)\n", szLevel, functionName, szModule, szId, szInfo);
-			return status;
-		}
-
-		return status;
-	}
 }
 
 void SapManager_Init() {
-	 _CorW32_EnableKernelEventNotification();
+	_CorW32_EnableKernelEventNotification();
+	SapManager::SetDisplayStatusMode(SapManager::StatusLog);
 }
 
 int SapManager_GetServerCount() {
     return SapManager::GetServerCount();
 }
-
 
 SapLocationWrapper SapLocation_New(const char* acqServerName, int acqDeviceNumber) {
     return new SapLocation(acqServerName, acqDeviceNumber);
