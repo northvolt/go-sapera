e29709cf0ec95bb315bd60199389682fc4a2a744
Ron Evans <ron@hybridgroup.com>
2021-05-18 19:14:59 +0200

    mx4: add all missing metadata items

--- mx4.go
+++ mx4.go
@@ -6,7 +6,10 @@ package sapera
 #include "buffer.h"
 */
 import "C"
-import "unsafe"
+import (
+	"time"
+	"unsafe"
+)
 
 type MX4Metadata struct {
 	// C.MX4MetadataWrapper
@@ -21,10 +24,34 @@ func (mta MX4Metadata) Close() {
 	C.MX4Metadata_Close((C.MX4MetadataWrapper)(mta.p))
 }
 
+func (mta MX4Metadata) ShaftEncoderCount() int {
+	return int(((C.MX4MetadataWrapper)(mta.p)).shaftEncoderCount)
+}
+
+func (mta MX4Metadata) LineCount() int64 {
+	return int64(((C.MX4MetadataWrapper)(mta.p)).lineCount)
+}
+
+func (mta MX4Metadata) LineTriggerCount() int64 {
+	return int64(((C.MX4MetadataWrapper)(mta.p)).lineTriggerCount)
+}
+
+func (mta MX4Metadata) Timestamp() time.Time {
+	return time.Unix(int64(((C.MX4MetadataWrapper)(mta.p)).timeStamp), 0)
+}
+
 func (mta MX4Metadata) FrameCounter() int {
 	return int(((C.MX4MetadataWrapper)(mta.p)).frameCounter)
 }
 
-func (mta MX4Metadata) LineCount() int {
-	return int(((C.MX4MetadataWrapper)(mta.p)).lineCount)
+func (mta MX4Metadata) GeneralInputs() byte {
+	return byte(((C.MX4MetadataWrapper)(mta.p)).generalInputs)
+}
+
+func (mta MX4Metadata) GeneralOutputs() byte {
+	return byte(((C.MX4MetadataWrapper)(mta.p)).generalOutputs)
+}
+
+func (mta MX4Metadata) BiDirectionalIOs() byte {
+	return byte(((C.MX4MetadataWrapper)(mta.p)).biDirectionalIOs)
 }
