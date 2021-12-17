ab2bf27801ecbeefd132da59ea091191a32d7627
Ron Evans <ron@hybridgroup.com>
2021-12-17 17:26:46 +0100

    mx4: clarify docs and params being used to parse returned metadata

--- mx4.go
+++ mx4.go
@@ -11,15 +11,20 @@ import (
 	"unsafe"
 )
 
+// MX4Metadata is the metadata such as the line count and encoder count for each
+// captured frame.
 type MX4Metadata struct {
 	// C.MX4MetadataWrapper
 	p unsafe.Pointer
 }
 
-func GetMX4MetadataFromBuffer(buf SapBuffer, width, height int) MX4Metadata {
-	return MX4Metadata{p: unsafe.Pointer(C.GetMX4MetadataFromBuffer((C.SapBufferWrapper)(buf.p), C.int(width), C.int(height)))}
+// GetMX4MetadataFromBuffer returns the MX4Metadata for a buffer. Pass in the width of the
+// image part of the buffer, and the line number (1-indexed) for which you want the metadata.
+func GetMX4MetadataFromBuffer(buf SapBuffer, width, line int) MX4Metadata {
+	return MX4Metadata{p: unsafe.Pointer(C.GetMX4MetadataFromBuffer((C.SapBufferWrapper)(buf.p), C.int(width), C.int(line)))}
 }
 
+// Close cleans up any memory that has been allocated for the MX4Metadata.
 func (mta MX4Metadata) Close() {
 	C.MX4Metadata_Close((C.MX4MetadataWrapper)(mta.p))
 }
